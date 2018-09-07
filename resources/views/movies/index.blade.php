80866227fd60e8ff19cc9c06399845e37438442b
Celaa <celaa@coches.com>
2018-09-08 00:17:55 +0200

    TASK-13: Mejorar la vista de la votacion
    Poner un poco mas bonica la vista

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -16,22 +16,22 @@
         </div>
         <div class="row margin-top-30">
             <div class="col-md-6">
-                <img height={{$movie->getPosterHeight()}}"700px" width={{$movie->getPosterWidth()}}"500px" src={!! html_entity_decode($movie->getPoster()) !!} />
+                <img class="border border-primary box-shadow" height={{$movie->getPosterHeight()}}"700px" width={{$movie->getPosterWidth()}}"500px"
+                     src={!! html_entity_decode($movie->getPoster()) !!} />
             </div>
-            <br/>
             <div class="col-md-6">
+                <h2><i class="material-icons">comment</i> Sinopsis</h2>
                 {{ $movie->getSummary()}}
                 <div>
-                    <iframe width="560" height="315" src="{{$movie->getTrailer()}}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <iframe style="margin-top:10px; border: 10px double #ddd;" width="560" height="315" src="{{$movie->getTrailer()}}" frameborder="0"
+                            allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
         <div class="row margin-top-20">
-            <div class="col-md-5">
-            </div>
-            <div class="col-md-2"></div>
-            <div class="col-md-5">
-                <h2>Votaciones</h2>
+            <div class="col-md-6"></div>
+            <div class="col-md-6">
+                <h2><i class="material-icons">thumb_up</i> Votaciones</h2>
                 <form method="post">
                     {{ csrf_field() }}
                     @if ($errors->any())
@@ -43,13 +43,13 @@
                     <span><strong><?php echo $users[$i]?></strong></span>
                     <fieldset class="margin-left-15">
                         <label for="vote_0<?=$i?>">
-                            <input type="radio" id="vote0_<?=$i?>" name="user_<?= $i ?>" class="vote" value="0" checked>&nbsp;No querer
+                            <input type="radio" id="vote0_<?=$i?>" name="user_<?= $i ?>" class="vote btn btn-secondary" value="0" checked>&nbsp;No querer
                         </label>
                         <label for="vote_1<?=$i?>">
-                            <input type="radio" id="vote1_<?=$i?>" name="user_<?= $i ?>" class="vote" value="1" {{$movie->isWeakScore($i)}}>&nbsp;Podría verla
+                            <input type="radio" id="vote1_<?=$i?>" name="user_<?= $i ?>" class="vote btn btn-secondary" value="1" {{$movie->isWeakScore($i)}}>&nbsp;Podría verla
                         </label>
                         <label for="vote_2<?=$i?>">
-                            <input type="radio" id="vote2_<?=$i?>" name="user_<?= $i ?>" class="vote" value="2" {{$movie->isStrongScore($i)}}>&nbsp;Quiero verla!!
+                            <input type="radio" id="vote2_<?=$i?>" name="user_<?= $i ?>" class="vote btn btn-secondary" value="2" {{$movie->isStrongScore($i)}}>&nbsp;Quiero verla!!
                         </label>
                     </fieldset>
                     <?php endfor; ?>
