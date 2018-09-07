dc436b78ae973e559c4cd27a617be20035938731
Celaa <celaa@coches.com>
2018-09-08 00:03:48 +0200

    TASK-12: Descartar pelis de brigadoon
    Descartas pelis de brigadoon en la importacion. Añadir README para la instalacion del proyecto

--- app/UseCases/ExtractMovieList/SitgesWeb2018MovieExtractor.php
+++ app/UseCases/ExtractMovieList/SitgesWeb2018MovieExtractor.php
@@ -2,10 +2,12 @@
 namespace App\UseCases\ExtractMovieList;
 
 use App\Helpers\FinderVideoService;
+use siesta\domain\exception\MovieNotForVoteException;
 use siesta\domain\extraction\MovieExtractor;
 use siesta\domain\movie\Movie;
 use siesta\infrastructure\movie\http\HtmlParser;
 
+//TODO: tests a esta clase
 /**
  * Class SitgesWebMovieExtractor
  * @package App\UseCases\ExtractMovieList
@@ -38,14 +40,19 @@ class SitgesWeb2018MovieExtractor implements MovieExtractor
         $elements = $this->_htmlParser->getElementsByClass($url, self::MOVIE_ELEMENT_CLASS);
         $movieList = [];
         foreach ($elements as $domMovie) {
-            [$title, $link] = $this->_getTitleFromMovieElement($domMovie);
-            $movie = new Movie();
-            $movie->setTitle($title);
-            $movie->setTrailerId($this->_getTrailer($title));
-            $movie->setDuration($this->_getDuration($link));
-            $movie->setPoster($this->_getPosterFromMovieElement($domMovie, $title));
-            $movie->setSummary($this->_getSummary($link));
-            $movieList[] = $movie;
+            try {
+                $this->_checkForVotableMovie($domMovie);
+                [$title, $link] = $this->_getTitleFromMovieElement($domMovie);
+                $movie = new Movie();
+                $movie->setTitle($title);
+                $movie->setTrailerId($this->_getTrailer($title));
+                $movie->setDuration($this->_getDuration($link));
+                $movie->setPoster($this->_getPosterFromMovieElement($domMovie, $title));
+                $movie->setSummary($this->_getSummary($link));
+                $movieList[] = $movie;
+            } catch (MovieNotForVoteException $e) {
+                continue;
+            }
         }
 
         return $movieList;
@@ -72,6 +79,7 @@ class SitgesWeb2018MovieExtractor implements MovieExtractor
     }
 
     /**
+     * @param string $link
      * @return int
      */
     private function _getDuration(string $link): int
@@ -88,6 +96,7 @@ class SitgesWeb2018MovieExtractor implements MovieExtractor
      * @return string
      */
     private function _getPosterFromMovieElement(\DiDom\Element $movie, $title) //TODO: sacar a clase dominio
+    : string
     {
         $externalPoster = $this->_tryWithCineMaterial($title);
         if (!empty($externalPoster)) {
@@ -101,6 +110,7 @@ class SitgesWeb2018MovieExtractor implements MovieExtractor
     }
 
     /**
+     * @param string $link
      * @return string
      */
     private function _getSummary(string $link): string
@@ -108,24 +118,35 @@ class SitgesWeb2018MovieExtractor implements MovieExtractor
         $rawTextList = $this->_htmlParser->getElementsByClass($link, 'section_sinopsi');
         $rawText = current($rawTextList);
 
-        return trim($rawText->text());
+        return trim(str_replace('Sinopsis', '', $rawText->text()));
     }
 
     /**
      * @param string $title
      * @return string
      */
-    private function _tryWithCineMaterial($title)
+    private function _tryWithCineMaterial($title): string
     {
         $cineMaterialSerch = file_get_contents('https://www.cinematerial.com/search?q=' . urlencode($title));
         if (preg_match('#<img\ssrc="(https://cdn.cinematerial.com/p/30x/[^"]*)"#', $cineMaterialSerch, $matches)) {
             $image = str_replace('30x', '500x', $matches[1]);
             preg_match('/style="color:\s#8C8C8C;">([^<]*)<\/span>/', $cineMaterialSerch, $yearMatches);
-            if (\in_array(trim($yearMatches[1]), ["2017", "2018"], true)) {
+            if (\in_array(trim($yearMatches[1]), ['2017', '2018'], true)) {
                 return $image;
             }
         }
 
         return '';
+    }
+
+    /**
+     * @param \DiDom\Element $movie
+     * @throws MovieNotForVoteException
+     */
+    private function _checkForVotableMovie(\DiDom\Element $movie): void
+    {
+        if (preg_match('/<p>Brigadoon<\/p>/', $movie->html())) {
+            throw new MovieNotForVoteException();
+        }
     }
 }
